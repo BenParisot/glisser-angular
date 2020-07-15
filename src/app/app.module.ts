c595f596644bc716bef17ae8e974a956c779c619
Ben Parisot <me@benparisot.com>
2020-07-14 20:25:17 -0700

    adds form field to module card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { EditModalComponent } from './edit-modal/edit-modal.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditModalComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
